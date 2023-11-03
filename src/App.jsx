d7cea6924fae051f5115a0cf4cbfce43a07982f6
nurullah91 <mdnurullah.ptk@gmail.com>
2023-11-03 22:49:22 +0600

    image upload function and delete function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import Gallery from './Components/Gallery'
 
 function App() {
 
   return (
-   <>
-   <h2 className="text-3xl text-center text-slate-600 font-bold mt-5">Welcome to Gallery</h2>
-   
-   </>
+    <>
+      <h2 className="text-3xl text-center text-slate-600 font-bold mt-5">Welcome to Gallery</h2>
+      <Gallery></Gallery>
+    </>
   )
 }
 
--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,9 +1,61 @@
-
+import { useState } from "react";
+import images from ""
 const Gallery = () => {
+
+    // thumbnail related states
+    const [selectThumbnails, setSelectThumbnails] = useState([]);
+    const [thumbnails, setThumbnails] = useState(images)
+
+    // drugging states
+    const [drugging, setDrugging] = useState(false);
+    const [draggedImage, setDraggedImage] = useState(null);
+    const [draggedIndex, setDraggedIndex] = useState(null)
+
+
+    // handle New image upload functionality
+    const handleUploadedImage = (e) => {
+        const selectedFiles = e.target.files;
+
+        const newImage = Array.from(selectedFiles).map((file, index) => {
+            const id = thumbnail.length + index + 1;
+            const thumbnail = URL.createObjectURL(file);
+
+            return {id, thumbnail}
+        })
+
+        setThumbnails([...thumbnails, ...newImage])
+    }
+
+
+    // handle delete image 
+    const handleDeleteImage = () => {
+
+        // getting latest images
+        const updatedImages = thumbnails.filter(
+            (image) => !selectThumbnails.some((selected) => selected.id === image.id)
+          );
+
+          setThumbnails(updatedImages)
+          setSelectThumbnails([])
+    }
+
     return (
-        <div>
-            <h1>Gallery</h1>
-        </div>
+        <main className="min-h-screen w-screen flex flex-row items-center justify-center md:p-0 p-4">
+            <div className="flex flex-col gap-y-2">
+                {/* todo */}
+                {/* call galleryHeader */}
+
+                <hr />
+                <section className="h-full w-full p-6">
+                    <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 gap-6"
+                    // todo
+                    //   onDragOver={handleDragOver}
+                    >
+
+                    </div>
+                </section>
+            </div>
+        </main>
     );
 };
 
