fdf40b7706b0e4161c6fd7e733062f672b7b20d9
nurullah91 <mdnurullah.ptk@gmail.com>
2023-11-03 09:45:08 +0600

    initial setup done with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h2 className="text-3xl text-center text-slate-600 font-bold mt-5">Welcome to Gallery</h2>
+   </>
+  )
+}
+
+export default App
