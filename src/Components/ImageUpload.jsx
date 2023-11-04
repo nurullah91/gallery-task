899d5f0987ee3ad1d14a4412a9359ac4e517b363
nurullah91 <mdnurullah.ptk@gmail.com>
2023-11-04 17:13:26 +0600

    upload compo done

--- src/Components/ImageUpload.jsx
+++ src/Components/ImageUpload.jsx
@@ -1,8 +1,22 @@
-
-const ImageUpload = () => {
+/* eslint-disable react/prop-types */
+import placeholderImg from './placeholder.webp'
+const ImageUpload = ({ handleUploadedImage }) => {
     return (
-        <div>
-            Image Upload
+        <div className="relative border-2 border-dashed rounded-lg p-4 hover:bg-gray-50 transition-colors ease-linear">
+            <input
+                type="file"
+                multiple
+                name="uploadImage"
+                id="uploadImage"
+                className="absolute top-0 left-0 h-full w-full opacity-0 cursor-pointer"
+                title="Upload a image"
+                onChange={handleUploadedImage}
+            />
+
+            <div className="h-full w-full flex flex-col justify-center items-center gap-y-4">
+                <img src={placeholderImg} alt="Upload Placeholder" />
+                <span className="`whitespace-nowrap">Add Image</span>
+            </div>
         </div>
     );
 };
