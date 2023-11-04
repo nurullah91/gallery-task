be7dd98a1973d1aa4369eccc4fd1a91102df1979
nurullah91 <mdnurullah.ptk@gmail.com>
2023-11-04 16:51:12 +0600

    uncheck error solved

--- src/Components/DragDropOverlay.jsx
+++ src/Components/DragDropOverlay.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 
-const DragDropOverlay = () => {
+const DragDropOverlay = ({dragging, draggedIndex, image}) => {
     return (
-        <div>
+        dragging && Number(draggedIndex) === Number(image.id) && (
+        <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-white border-2 border-dashed rounded-lg z-50">
+            Drop Here
             
         </div>
+
+        )
     );
 };
 
--- src/Components/GalleryImage.jsx
+++ src/Components/GalleryImage.jsx
@@ -6,6 +6,7 @@ const GalleryImage = ({ image, index, selectThumbnails, setSelectThumbnails, han
 
     return (
         <div
+            key={index}
             className={`group relative before:content-[''] before:absolute before:h-full before:w-full before:rounded-lg before:transition-colors before:cursor-move ${(index === 0 ? "md:col-span-2 md:row-span-2" : "col-span-1")} ${(selectThumbnails.find((photo) => photo.id === image.id) ? "opacity-100" : "hover:before:bg-black/50")}`}
             draggable={true}
             onDragStart={() => handleDragStart(image)}
@@ -13,7 +14,11 @@ const GalleryImage = ({ image, index, selectThumbnails, setSelectThumbnails, han
         >
             <img src={image.image} alt="Image" className={`h-full w-full max-w-full rounded-lg object-contain border-2 ${selectThumbnails.find((photo) => photo.id === image.id) && "opacity-30"}`} />
 
-            <input type="checkBox" name={image.id} id={image.id} className={`absolute top-4 left-4 h-4 w-4 accent-blue-700 group-hover:opacity-100 transition-all delay-100 duration-200 ease-linear cursor-pointer ${selectThumbnails.find((photo) => photo.id === image.id) ? "opacity-100" : "opacity-0"}`}
+            <input
+             type="checkBox" 
+             name={image.id} 
+             id={image.id} 
+             className={`absolute top-4 left-4 h-4 w-4 accent-blue-700 group-hover:opacity-100 transition-all delay-75 duration-200 ease-linear cursor-pointer ${selectThumbnails.find((photo) => photo.id === image.id) ? "opacity-100" : "opacity-0"}`}
 
                 checked={
                     selectThumbnails.find((photo) => photo.id === image.id) ? true : false
@@ -21,7 +26,9 @@ const GalleryImage = ({ image, index, selectThumbnails, setSelectThumbnails, han
 
                 onChange={() => {
                     if (selectThumbnails.find((photo) => photo.id === image.id))
-                        setSelectThumbnails(setSelectThumbnails.filter((photo) => photo.id !== image.id))
+                    setSelectThumbnails(
+                        selectThumbnails.filter((photo) => photo.id !== image.id)
+                      )
                     else setSelectThumbnails([...selectThumbnails, image])
                 }}
             />
